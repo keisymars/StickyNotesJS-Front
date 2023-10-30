5e4276e608f42374cbb5f43f9bf06bc7331dbb00
Keisy <keisy.nascimento@hotmail.com>
2023-10-30 03:03:12 -0300

    cria site denotas adesivas

---
+++ animations.js
@@ -0,0 +1,9 @@
+/*
+let createBox = document.getElementsByClassName('createBox')[0];
+let notes = document.getElementsByClassName('notes')[0];
+let input = document.getElementById('userInput');
+let i = 0;
+document.getElementById("create").addEventListener("click", function(){
+   createBox.style.display = "block";
+
+});
